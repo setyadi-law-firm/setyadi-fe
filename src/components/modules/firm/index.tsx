0a7ef2d91c79f78e6e8d50cff0b6d8e2e952ad76
CyberSleeper <mahartha.gemilang@gmail.com>
2025-04-30 16:27:50 +0700

    feat: add vision section

--- src/components/modules/firm/index.tsx
+++ src/components/modules/firm/index.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "./hero";
+import { VisionSection } from "./vision";
 
 export function FirmPageModule() {
   return (
     <div className="items-center w-full z-0 min-h-screen relative justify-items-center gap-16">
       <HeroSection />
+      <VisionSection />
     </div>
   );
 }
---
+++ src/components/modules/firm/vision/index.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+
+export function VisionSection() {
+  return (
+    <div className="items-center w-full z-0 relative flex py-20 px-20 justify-items-center gap-16">
+      <div className="md:w-2/3 flex flex-col">
+        <h3 className="text-xl text-[#1059BD] font-semibold mb-8">
+          Trusted Lawyers with a Vision for Excellence
+        </h3>
+        <h4 className="text-lg text-neutral-950 font-semibold mb-4">
+          Our Mission
+        </h4>
+        <p className="text-[#777675]">
+          To provide trusted, effective, and timely legal solutions that help
+          our clients achieve their goals — whether navigating regulatory
+          frameworks, resolving disputes, or managing risk in an evolving
+          market.
+        </p>
+      </div>
+      <div className="md:w-1/3 flex flex-col items-center">
+        <p className="text-[#777675] font-semibold mb-4">
+          Our team of experts has extensive experience across various areas of
+          law
+        </p>
+        <Button
+          variant={"default"}
+          className="bg-[#1059BD] text-white hover:bg-[#0e4da8] w-full mt-4"
+          onClick={() =>
+            window.open("https://www.linkedin.com/company/legally-legal/")
+          }
+        >
+          Explore Our Team
+        </Button>
+      </div>
+    </div>
+  );
+}
