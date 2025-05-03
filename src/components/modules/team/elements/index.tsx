2bef4543980ff80296966b6e8d4c0dc4ebcb55f0
CyberSleeper <mahartha.gemilang@gmail.com>
2025-05-03 09:45:33 +0700

    feat: add our team mobile

--- src/components/modules/team/elements/index.tsx
+++ src/components/modules/team/elements/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { Mail } from "lucide-react";
 import Image from "next/image";
 
 export interface TeamMemberCardProps {
@@ -16,8 +18,8 @@ export const TeamMemberCard = ({
   description,
 }: TeamMemberCardProps) => {
   return (
-    <div className="flex h-72 gap-8 w-full p-4">
-      <div className="relative rounded-sm shrink-0 aspect-[3/4] bg-red-500 h-full mb-4 overflow-hidden shadow-lg">
+    <div className="flex max-md:flex-col md:h-72 gap-x-8 w-full p-4">
+      <div className="relative rounded-sm shrink-0 aspect-[3/4] bg-red-500 h-full mb-4 overflow-clip shadow-lg max-md:w-2/3">
         <Image
           src={imageUrl}
           alt={name}
@@ -26,13 +28,25 @@ export const TeamMemberCard = ({
           className="mb-4 rounded-md"
         />
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-6 max-md:gap-3">
         <h3 className="flex flex-col gap-1">
-          <span className="text-4xl font-bold text-neutral-950">{name}</span>
-          <span className="text-2xl font-bold text-[#1059BD]">{role}</span>
-          <span className="text-base text-[#777675]">{email}</span>
+          <span className="md:text-4xl text-2xl font-bold text-neutral-950">
+            {name}
+          </span>
+          <span className="md:text-2xl text-2xl font-bold text-[#1059BD]">
+            {role}
+          </span>
+          <Button
+            variant={"ghost"}
+            className="md:text-base text-lg flex items-center gap-2 text-[#777675] w-fit"
+            onClick={() => {
+              window.open(`mailto:${email}`, "_blank");
+            }}
+          >
+            <Mail /> <p>{email}</p>
+          </Button>
         </h3>
-        <p className="text-base text-[#777675]">{description}</p>
+        <p className="md:text-base text-base text-[#777675]">{description}</p>
       </div>
     </div>
   );
