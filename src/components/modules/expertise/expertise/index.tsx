e6db7c3164d278219175b836a3af53f958158790
CyberSleeper <mahartha.gemilang@gmail.com>
2025-06-07 11:41:35 +0700

    feat: add button to redirect to our expertise

--- src/components/modules/expertise/expertise/index.tsx
+++ src/components/modules/expertise/expertise/index.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 
 export function ExpertiseSection() {
   return (
-    <div className="items-center w-full z-0 relative gap-16">
-      <div className="w-full pb-12 relative -z-10 pt-20 md:px-20 px-8">
+    <div
+      className="items-center w-full z-0 relative gap-16 pt-24"
+      id="expertise"
+    >
+      <div className="w-full pb-12 relative -z-10 md:px-20 px-8">
         <div className="py-3 text-neutral-950 font-bold text-base md:text-4xl">
           Our Expertise
         </div>
--- src/components/modules/home/expertise/index.tsx
+++ src/components/modules/home/expertise/index.tsx
@@ -1,33 +1,46 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 export function ExpertiseSection() {
+  const router = useRouter();
+
   const EXPERTISES = [
-    "Litigation",
-    "Corporate Law",
-    "Intellectual Property",
-    "Litigation and Dispute Resolution",
-    "Employment Law",
-    "Real Estate",
-    "Tax Law",
-    "Family Law",
+    "Energy Law",
+    "Business Legality",
+    "Policy Advice",
+    "Constitutional Review",
   ];
 
   return (
-    <div className="max-md:px-12 max-md:py-12 px-16 items-center text-center py-18 bg-neutral-50 text-neutral-950 w-full z-0 relative flex flex-col gap-8">
+    <div className="max-md:px-12 max-md:py-12 px-16 items-center text-center py-18 bg-neutral-50 text-neutral-950 w-full z-0 relative flex flex-col gap-8 mb-16">
       <h1 className="md:text-4xl text-3xl font-bold">Our Expertise</h1>
-      <p>
+      <p className="md:text-base text-sm text-neutral-950 leading-relaxed max-w-2xl mx-auto">
         We provide a wide range of legal services designed to support your
         business needs. Our team of experts has extensive experience across
         various areas of law, including:
       </p>
-      <div className="flex flex-wrap w-fit items-center justify-center gap-4 md:mt-10 mt-4">
+      <div className="flex-wrap items-center justify-center gap-4 grid grid-cols-4 w-2/3">
         {EXPERTISES.map((expertise, index) => (
           <div
             key={index}
-            className="bg-neutral-900 text-neutral-50 p-4 flex items-center justify-center text-center"
+            className="bg-[#0F172A] text-neutral-50 p-4 flex items-center justify-center text-center h-full"
           >
             {expertise}
           </div>
         ))}
-      </div>
+      </div>{" "}
+      <Button
+        variant="outline"
+        className="max-md:w-full max-md:text-sm md:text-base text-[#1059BD] border-[#1059BD] hover:bg-[#1059BD] hover:text-white rounded-xl !px-8 py-6"
+        onClick={() => {
+          router.push("/expertise#expertise");
+        }}
+      >
+        See Details <ArrowRight className="w-4 h-4 ml-2" />
+      </Button>
     </div>
   );
 }
