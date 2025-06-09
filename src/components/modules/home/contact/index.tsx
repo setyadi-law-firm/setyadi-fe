c357b0b36d8397382ce93911a83747a30e0ed3ef
CyberSleeper <mahartha.gemilang@gmail.com>
2025-06-09 15:17:41 +0700

    fix: add mailto to contact section

--- src/components/modules/home/contact/index.tsx
+++ src/components/modules/home/contact/index.tsx
@@ -19,7 +19,12 @@ export function ContactSection() {
       </div>
       <div className="min-h-screen w-full text-neutral-50 gap-7 flex items-center justify-center flex-col text-center pt-20 z-10">
         <h1 className="md:text-5xl text-3xl font-bold">Contact Us</h1>
-        <div className="border-2 border-neutral-50 cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out text-white font-bold py-3 px-8 rounded-xl">
+        <div
+          onClick={() => {
+            window.open(`mailto:setyadiandpartner@gmail.com`, "_blank");
+          }}
+          className="border-2 border-neutral-50 cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out text-white font-bold py-3 px-8 rounded-xl"
+        >
           Reach Us
         </div>
       </div>
